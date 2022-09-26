8745267e9d6fc2f10be6d34942296d6a4520867d
Ayrat Khayretdinov <ayrat.khayretdinov@gmail.com>
2022-09-26 17:57:26 -0400

    module 2 labs

---
+++ tf-samples/terraform-basics-3/uat.tfvars
@@ -0,0 +1,5 @@
+region="us-central1"
+project=PROJECT_ID
+name="flask-server-uat"
+machine_type="f1-micro"
+zone="us-central1-c"
---
+++ tf-samples/terraform-basics-3/variables.tf
@@ -0,0 +1,63 @@
+variable "region" {
+  default = "us-central1"
+}
+
+variable "project" {
+  default = "centos-cloud"
+}
+
+variable "zone" {
+  default = "us-central1-b"
+}
+
+variable "machine_type" {
+  description = "In the form of custom-CPUS-MEM, number of CPUs and memory for custom machine. https://cloud.google.com/compute/docs/instances/creating-instance-with-custom-machine-type#specifications"
+  default     = "f1-micro"
+}
+
+variable "name" {
+  description = "Name prefix for the nodes"
+  default     = "flask-vm"
+}
+
+/*
+variable "disk_auto_delete" {
+  description = "Whether or not the disk should be auto-deleted."
+  default     = true
+}
+
+variable "disk_type" {
+  description = "The GCE disk type. Can be either pd-ssd, local-ssd, or pd-standard."
+  default     = "pd-ssd"
+}
+
+
+variable "startup_script" {
+  description = "Content of startup-script metadata passed to the instance template."
+  default     = ":"
+}
+
+variable "metadata" {
+  description = "Map of metadata values to pass to instances."
+  type        = "map"
+  default     = {}
+}
+
+variable "service_account_email" {
+  description = "The email of the service account for the instance template."
+  default     = ""
+}
+
+variable "service_account_scopes" {
+  description = "List of scopes for the instance template service account"
+  type        = "list"
+
+  default = [
+    "https://www.googleapis.com/auth/compute",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring.write",
+    "https://www.googleapis.com/auth/devstorage.full_control",
+  ]
+}
+*/
+
